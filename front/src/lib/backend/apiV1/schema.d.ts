f01215ac9b8cf70bd6d57589878203a1efd8a327
bighead0831 <bighead0831@naver.com>
2026-06-23 15:08:39 +0900

    011-2

--- front/src/lib/backend/apiV1/schema.d.ts
+++ front/src/lib/backend/apiV1/schema.d.ts
@@ -228,7 +228,6 @@ export interface components {
             authorId?: number;
             authorName?: string;
             title?: string;
-            content?: string;
         };
         RsDataPostDto: {
             resultCode?: string;
@@ -290,6 +289,19 @@ export interface components {
             msg?: string;
             data?: components["schemas"]["MemberLoginResBody"];
         };
+        PostWithContentDto: {
+            /** Format: int32 */
+            id?: number;
+            /** Format: date-time */
+            createDate?: string;
+            /** Format: date-time */
+            modifyDate?: string;
+            /** Format: int32 */
+            authorId?: number;
+            authorName?: string;
+            title?: string;
+            content?: string;
+        };
         MemberWithUsernameDto: {
             /** Format: int32 */
             id?: number;
@@ -297,8 +309,8 @@ export interface components {
             createDate?: string;
             /** Format: date-time */
             modifyDate?: string;
-            username?: string;
             name?: string;
+            username?: string;
         };
         AdmPostCountResBody: {
             /** Format: int64 */
@@ -403,7 +415,7 @@ export interface operations {
                     [name: string]: unknown;
                 };
                 content: {
-                    "application/json;charset=UTF-8": components["schemas"]["PostDto"];
+                    "application/json;charset=UTF-8": components["schemas"]["PostWithContentDto"];
                 };
             };
         };
